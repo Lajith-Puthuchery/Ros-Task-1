a9db27e0f1957f0b41364d0aad32ff26d97b295b
Lajith Puthuchery <lajith.p@gmail.com>
2021-02-17 11:17:18 +0530

    Workspace for ROS Task #1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lajith/simplebot_ws/src
+# Build directory: /home/lajith/simplebot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("firstbot")
---
+++ build/firstbot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lajith/simplebot_ws/src/firstbot
+# Build directory: /home/lajith/simplebot_ws/build/firstbot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
